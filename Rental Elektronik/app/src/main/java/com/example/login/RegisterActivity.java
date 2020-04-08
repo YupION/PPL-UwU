bfd654f5a97c7d1685f15ba7d3018369c3613907
alvin <alvinkurniawan2708@gmail.com>
2020-04-08 22:04:30 +0700

    Update Register.java

--- Rental Elektronik/app/src/main/java/com/example/login/RegisterActivity.java
+++ Rental Elektronik/app/src/main/java/com/example/login/RegisterActivity.java
@@ -3,12 +3,23 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText nama,nik,username,telp,alamat,email,password,c_password,kelamin;
+    private Button btn_regist;
+
+    private static String URL_REGIST = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        nama = findViewById(R.id.et_nama);
+
+
     }
 }
