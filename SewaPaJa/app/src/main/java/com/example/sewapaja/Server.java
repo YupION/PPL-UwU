ee0e2ff4e388df65b742023d0710c09dbf1db3e3
alvin <alvinkurniawan2708@gmail.com>
2020-04-15 20:33:05 +0700

    Update 15April

--- SewaPaJa/app/src/main/java/com/example/sewapaja/Server.java
+++ SewaPaJa/app/src/main/java/com/example/sewapaja/Server.java
@@ -1,5 +1,5 @@
 package com.example.sewapaja;
 
 public class Server {
-    public static final String URL = "http://172.28.1.22/sewa_barang/";
+    public static final String URL = "http://192.168.1.4/sewa_barang/";
 }
